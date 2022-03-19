639d2cd80c2285d3331fd6ca1c5f618c2ee74659
Craig Mellor <neskechastro@gmail.com>
2022-03-19 01:51:13 -0400

    Poly Count Decider

--- Processing.cs
+++ Processing.cs
@@ -4,7 +4,7 @@ namespace g3{
 
     class Processing{
         public static void Main(){
-            const string in_path = "Raw-Training/";
+            const string in_path = "../Raw-Training/";
             const string out_path = "Processed_Training";
             const int new_vertex_count = 1000;
  
---
+++ Processor/Program.cs
@@ -0,0 +1,19 @@
+﻿using g3;
+namespace Main{
+
+    class Processing{
+        public static void Main(){
+            const string in_path = "/Raw-Training/one.obj";
+            const string out_path = "/Processed-Training/one.obj";
+            const int new_vertex_count = 1000;
+ 
+            DMesh3 mesh = StandardMeshReader.ReadMesh(in_path);
+            Console.WriteLine("HEHHEHE" + mesh);
+            Reducer r = new Reducer(mesh);
+            r.ReduceToVertexCount(new_vertex_count);
+            IOWriteResult result = StandardMeshWriter.WriteFile(out_path,
+            new List<WriteMesh>() { new WriteMesh(mesh) }, WriteOptions.Defaults);
+         
+        }
+    }
+}
